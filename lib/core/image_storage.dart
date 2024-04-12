eb461d629b3b0847c1b6beac5151c576a3bbd45c
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-12 09:29:38 +0900

    이미지 추가에 따른 이미지 관련 위젯을 생성

---
+++ lib/core/image_storage.dart
@@ -0,0 +1,6 @@
+String _imagePath = "assets/image/";
+
+class ImageStorage {
+  static String awackLogo = "${_imagePath}awack_logo.png";
+  static String awackTextLogo = "${_imagePath}awack_text_logo.png";
+}
