fb68aa5d174d552f0c3ab9f4a900c115a2207f06
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-12 14:41:11 +0900

    left_arrow_ios 이미지 추가

--- lib/core/image_storage.dart
+++ lib/core/image_storage.dart
@@ -3,4 +3,5 @@ String _imagePath = "assets/image/";
 class ImageStorage {
   static String awackLogo = "${_imagePath}awack_logo.png";
   static String awackTextLogo = "${_imagePath}awack_text_logo.png";
+  static String leftArrowIos = "${_imagePath}left_arrow_ios.png";
 }
