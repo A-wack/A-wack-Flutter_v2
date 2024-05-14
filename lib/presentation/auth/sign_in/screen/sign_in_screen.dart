4a281347b60ee058459c8b3d6a7eb303e8ea106d
ljyo2o9 <yggosxoljy@gmail.com>
2024-05-14 11:22:39 +0900

    AWackTextField에서 비밀번호를 위한 설정 추가 + SingleChildScrollView추가

--- lib/presentation/auth/sign_in/screen/sign_in_screen.dart
+++ lib/presentation/auth/sign_in/screen/sign_in_screen.dart
@@ -1,9 +1,11 @@
 import 'package:a_wack_flutter_v2/core/component/a_wack_text_field.dart';
 import 'package:a_wack_flutter_v2/core/component/text_widget.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:a_wack_flutter_v2/presentation/auth/sign_in/widget/sign_in_app_bar_widget.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/component/image_storage.dart';
+import '../../../../core/component/image_widget.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -18,6 +20,8 @@ class _SignInScreenState extends State<SignInScreen> {
 
   final FocusNode emailNode = FocusNode();
   final FocusNode passwordNode = FocusNode();
+
+  bool isInvisible = true;
 
   @override
   void initState() {
@@ -47,38 +51,87 @@ class _SignInScreenState extends State<SignInScreen> {
     _passwordController.dispose();
   }
 
+  void clickInvisible() {
+    isInvisible ? isInvisible = false : isInvisible = true;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
+    // todo 여기 있는 더미값 수정
+    bool isError = true;
+
     return SafeArea(
       child: Scaffold(
         appBar: SignInAppBarWidget(appbarHeight: 54.h),
         body: Padding(
           padding: EdgeInsets.fromLTRB(24.w, 36.h, 24.w, 0.h),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextWidget.bold(
-                text: '로그인',
-                fontSize: 32.sp,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextWidget.bold(
+                  text: '로그인',
+                  fontSize: 32.sp,
+                ),
+                SizedBox(height: 36.h),
+                AWackTextField(
+                  controller: _emailController,
+                  title: "이메일",
+                  focusNode: emailNode,
+                  keyboardType: TextInputType.emailAddress,
+                  isError: isError,
+                ),
+                SizedBox(height: 24.h),
+                AWackTextField(
+                  controller: _passwordController,
+                  title: "비밀번호",
+                  focusNode: passwordNode,
+                  isError: isError,
+                  errorText: "이메일 또는 비밀번호를 확인해주세요",
+                  keyboardType: TextInputType.visiblePassword,
+                  isInvisible: isInvisible,
+                  onTapOutside: (event) {
+                    passwordNode.unfocus();
+                    setState(() {});
+                  },
+                  suffixIcon: GestureDetector(
+                    onTap: () => clickInvisible(),
+                    child: ImageWidget(
+                      imageType: ImageType.png,
+                      image: isInvisible
+                          ? ImageStorage.hide
+                          : ImageStorage.notHide,
+                      width: 24.w,
+                      height: 24.h,
+                      imageWidth: 24.w,
+                      imageHeight: 24.h,
+                      color: passwordNode.hasFocus
+                          ? Colors.black
+                          : const Color(0xFFC1C1C1),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomSheet: Padding(
+          padding: EdgeInsets.only(bottom: 30.h),
+          child: Container(
+            width: 240.w,
+            height: 60.h,
+            decoration: BoxDecoration(
+              color: const Color(0xFFFFBD43),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Center(
+              child: TextWidget.semiBold(
+                text: "로그인",
+                fontSize: 18.sp,
               ),
-              SizedBox(height: 36.h),
-              AWackTextField(
-                controller: _emailController,
-                title: "이메일",
-                focusNode: emailNode,
-                anotherNodes: [passwordNode],
-                isError: false,
-              ),
-              SizedBox(height: 24.h),
-              AWackTextField(
-                controller: _passwordController,
-                title: "비밀번호",
-                focusNode: passwordNode,
-                anotherNodes: [emailNode],
-                isError: true,
-              ),
-            ],
+            ),
           ),
         ),
       ),
--- lib/presentation/on_boarding/screen/on_boarding_screen.dart
+++ lib/presentation/on_boarding/screen/on_boarding_screen.dart
@@ -10,6 +10,7 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Column(
         children: [
           Padding(
