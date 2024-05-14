51f00067f1c7caea4917508c03dd2469038a6a73
ljyo2o9 <yggosxoljy@gmail.com>
2024-05-14 11:17:32 +0900

    이미지 추가

--- lib/core/component/image_storage.dart
+++ lib/core/component/image_storage.dart
@@ -4,4 +4,6 @@ class ImageStorage {
   static String awackLogo = "${_imagePath}awack_logo.png";
   static String awackTextLogo = "${_imagePath}awack_text_logo.png";
   static String leftArrowIos = "${_imagePath}left_arrow_ios.png";
+  static String hide = "${_imagePath}hide.png";
+  static String notHide = "${_imagePath}notHide.png";
 }
